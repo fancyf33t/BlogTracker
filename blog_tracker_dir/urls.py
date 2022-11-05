4b1615d46cbe9fdfda426677cd8f815b3659e6c7
fancyf33t <fancyf33t@protonmail.com>
2022-11-05 12:02:18 -0500

    single page topic

--- blog_tracker_dir/urls.py
+++ blog_tracker_dir/urls.py
@@ -8,4 +8,8 @@ app_name = 'blog_tracker_dir'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Topics pages
+    path('topics/', views.topics, name='topics'),
+    # Details for single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
